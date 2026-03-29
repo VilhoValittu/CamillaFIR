a2881d7939ebfe0a450a3bed9df473610af543b8
VilhoValittu <ville@villef.org>
2026-03-29 22:27:03 +0300

    pywebio_hook remove

--- tests/test_pywebio_hook_assets.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-
-def test_pywebio_hook_includes_default_theme_css():
-    hook_path = Path(__file__).resolve().parents[1] / "pyinstaller_hooks" / "hook-pywebio.py"
-    hook_text = hook_path.read_text(encoding="utf-8")
-
-    assert '"html/css/bs-theme/default.min.css"' in hook_text
-    assert '"html/css/bs-theme/dark.min.css"' in hook_text
